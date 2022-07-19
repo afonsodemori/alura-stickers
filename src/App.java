babc76bc9f8c5350aebf5a599392bd2c757e61c8
Afonso de Mori <me@afonso.dev>
2022-07-19 05:43:17 +0200

    feat: Challenge 001 - Allow the user to select other endpoints

--- src/App.java
+++ src/App.java
@@ -2,15 +2,17 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class App {
     private static final String API_TOKEN = "some_token_here";
 
     public static void main(String[] args) throws Exception {
         // Endpoint to connect to
-        String url = "https://imdb-api.com/en/API/Top250Movies/" + API_TOKEN;
+        String url = selectEndpoint();
 
         // Generating and sending the Request
         URI uri = URI.create(url);
@@ -30,5 +32,44 @@ public class App {
             System.out.println(movie.get("imDbRating"));
             System.out.println();
         }
+    }
+
+    /**
+     * CHALLENGE 001: Consume the most popular movies' endpoint from IMDB API. Also look in the IMDB API documentation
+     * for the endpoint that returns the best series and the one that returns the most popular series.
+     */
+    private static String selectEndpoint() {
+        System.out.print("""
+                Select an option:
+                [1] Top 250 Movies
+                [2] Most Popular Movies
+                [3] Top 250 TVs
+                [4] Most Popular TVs
+                >\s"""
+        );
+
+        List<String> options = new ArrayList<>();
+        options.add("Top250Movies");
+        options.add("MostPopularMovies");
+        options.add("Top250TVs");
+        options.add("MostPopularTVs");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int optionIndex = -1;
+        while (optionIndex == -1) {
+            String option = scanner.nextLine();
+            try {
+                optionIndex = Integer.parseInt(option) - 1;
+            } catch (NumberFormatException ignored) {
+            } finally {
+                if (optionIndex < 0 || optionIndex >= options.size()) {
+                    System.out.printf("Invalid option. Choose a number between 1 and %d:%n> ", options.size());
+                    optionIndex = -1;
+                }
+            }
+        }
+
+        return "https://imdb-api.com/en/API/%s/%s".formatted(options.get(optionIndex), API_TOKEN);
     }
 }
